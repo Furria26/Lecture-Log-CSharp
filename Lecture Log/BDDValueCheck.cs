91960c29cc58cd5a368eeb077979673d416bb2cf
Furria26 <dayetilan@gmail.com>
2025-02-27 12:20:02 +0100

    First commit.

---
+++ Lecture Log/BDDValueCheck.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using System.Data.SqlClient;
+
+namespace BDDValueCheck
+{
+    ////private static string query = "SELECT TDate FROM T_TRANS";
+    //private class CheckVal
+    //{
+    //    // Méthode pour vérifier le format de la date "ddMMyyyy"
+    //    public static bool CheckDate(string date)
+    //    {
+    //        // Vérifie si la date correspond au format "ddMMyyyy"
+    //        string pattern = @"^\d{6}$";  // 6 chiffres (jour, mois, année sous forme "ddMMyyyy")
+
+    //        if (Regex.IsMatch(date, pattern))
+    //        {
+    //            // Essayer de convertir la chaîne avec le format exact "ddMMyyyy"
+    //            return DateTime.TryParseExact(date, "yyMMdd", null, System.Globalization.DateTimeStyles.None, out _);
+    //        }
+    //        return false;
+    //    }
+    //}
+
+    public class CBDDValueCheck
+    {
+        private static void CheckVal()
+        {
+            string[] columns = ["TDate", "Remise", "Num", "TTime", "Approved", "Collecte", 
+                                "Amount", "Aid", "Pan", "Iso2", "TacIac", "Online", "Emv",
+                                "Prop", "PanHash", "Name", "Bank", "Tags", "IdVoie", "Smact", 
+                                "Timings"];
+
+            for (int i = 0; i < columns.Length; i++)
+            {
+                Console.WriteLine(columns[i]);
+            }
+
+
+        }
+
+        public static void MainBDD()
+        {
+            CheckVal();
+        }
+    }
+}
---
+++ Lecture Log/Program.cs
@@ -0,0 +1,210 @@
+﻿using System;
+using System.IO;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Text.RegularExpressions;
+using static System.Net.Mime.MediaTypeNames;
+using System.Data.SQLite;
+using System.Diagnostics;
+using Var;
+
+using BDDValueCheck;
+
+public class Program
+{
+    public static string fileData = new FileName().FILE_DATA;
+    public static string finalFile = new FileName().FINAL_FILE;
+    public static string fileBdd = new FileName().FILE_BDD;
+    public static string log_dir = new ConstantVar().LOG_DIRECTORY;
+    public static Regex recupTramePattern = new ConstantVar().RECUP_TRAME_PATTERN;
+    public static string[] bannedChar = new ConstantVar().BANNED_CHAR;
+
+    private static void CreateBdd()
+    {
+        string settings = "Data Source=log_info.db;Version=3";
+        Console.WriteLine(":: [*] BDD Connection.");
+        try
+        {
+            using (SQLiteConnection connection = new SQLiteConnection(settings))
+            {
+                connection.Open();
+                using (SQLiteCommand command = new SQLiteCommand(connection))
+                {
+                    command.CommandText = TblChar.CREATE_BDD;
+                    command.ExecuteNonQuery();
+                    Console.WriteLine(":: [+] BDD Connection OK !\r\n::");
+                    using (StreamReader reader = new StreamReader(finalFile))
+                    {
+                        string? line;
+                        Console.WriteLine(":: [*] Execute SQL queries.");
+                        // Lire chaque ligne du fichier
+                        while ((line = reader.ReadLine()) != null)
+                        {
+                            command.CommandText = line;
+                            command.ExecuteNonQuery();  // Exécuter la commande SQL
+                            //Console.WriteLine($"Requête exécutée : {line} \r\n");
+                        }
+                        Console.WriteLine(":: [+] Queries executed successfully !");
+                    }
+                }
+            }// Close BDD 
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(":: [-] " + e.Message);
+        }
+    }
+
+    private static void FileManagement()
+    {
+        Console.WriteLine(":: [*] Creating SQL queries.");
+        using (StreamReader reader = new StreamReader(fileData))
+        {
+            List<string> tblCommandSQL = new List<string>();
+            string? line;
+            while ((line = reader.ReadLine()) != null)
+            {
+                if (Regex.IsMatch(line, @"^insert"))
+                {
+                    // SI la taille == 0 ALORS j'ajoute la ligne dans le tableau 
+                    if (tblCommandSQL.Count == 0)
+                    {
+                        tblCommandSQL.Add(line);
+                    }
+                    else
+                    {
+                        string? commandSQL = string.Join("", tblCommandSQL);
+                        using (StreamWriter writer = new StreamWriter(finalFile, true))
+                        {
+                            writer.WriteLine(commandSQL);
+                        }
+                        tblCommandSQL.Clear();
+                        tblCommandSQL.Add(line);
+                    }
+                }
+                else
+                {
+                    tblCommandSQL.Add(line);
+                }
+            }
+            string? lastCommandSQL = string.Join("", tblCommandSQL);
+            using (StreamWriter writer = new StreamWriter(finalFile, true))
+            {
+                writer.WriteLine(lastCommandSQL);
+            }
+        }
+        Console.WriteLine(":: [+] SQL queries created !\r\n::");
+    }
+
+    private static void WriteLogFile(string lineLog, StreamWriter writer)
+    {
+        try
+        {
+            if (recupTramePattern.Match(lineLog).Success) // Sans .Success la condition retournerait un objet de type match et non un bool 
+            {
+                if (!lineLog.Contains(bannedChar[0]) &&
+                    !lineLog.Contains(bannedChar[1]) &&
+                    !lineLog.Contains(bannedChar[2]) &&
+                    !lineLog.Contains(bannedChar[3]) &&
+                    !lineLog.Contains("where Remise="))
+                {
+                    // Remplacer les caractères
+                    for (int count = 0; count < 4; count++)
+                    {
+                        lineLog = Regex.Replace(lineLog, TblChar.SEARCH_CHAR[count], TblChar.REPLACE_CHAR[count]);
+                    }
+                    if (!Regex.IsMatch(lineLog, @"^Emv =") && !Regex.IsMatch(lineLog, @"^Collecte=\d{2} where"))
+                    {
+                        for (int count = 3; count < TblChar.SEARCH_CHAR.Length; count++)
+                        {
+                            lineLog = Regex.Replace(lineLog, TblChar.SEARCH_CHAR[count], TblChar.REPLACE_CHAR[count]);
+                        }
+                        writer.WriteLine(lineLog.Trim());
+                    }
+                }
+            }
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine($":: [-] Erreur lors de l'écriture : {e.Message}");
+        }
+    }
+
+    private static void ReadAllFile()
+    {
+        if (Directory.Exists(log_dir))
+        {
+            // Parcourir tous les fichiers du dossier
+            string[] fichiers = Directory.GetFiles(log_dir);
+
+            using (StreamWriter writer = new StreamWriter(fileData, true)) // Ouvre le fichier une seule fois
+            {
+                Console.WriteLine(":: [*] Reading files.");
+                foreach (string fichier in fichiers)
+                {
+                    //Console.WriteLine($"Traitement du fichier : {fichier}");
+                    using (StreamReader reader = new StreamReader(fichier))
+                    {
+                        //Console.WriteLine($":: Lecture du fichier : {fichier}"); 
+                        string? line;
+                        // Lire chaque ligne du fichier
+                        while ((line = reader.ReadLine()) != null)
+                        {
+                            WriteLogFile(line, writer);
+                        }
+                    }
+                }
+                Console.WriteLine(":: [+] Files are read !\r\n::");
+            }
+        }
+        else
+        {
+            Console.WriteLine(":: [-] The 'log' folder doesn't exist.");
+        }
+    }
+
+    private static void OutputTitle()
+    {
+        Console.WriteLine("::---------------------------------------------------------------------------------------------------");
+        Console.WriteLine(":: Lecture Log - (C) Vinci-Autoroutes");
+        Console.WriteLine("::---------------------------------------------------------------------------------------------------");
+    }
+
+    private static void DisplayRuntime(TimeSpan elapsed)
+    {
+        // Affiche le temps écoulé en format minute:seconde:milliseconde
+        Console.WriteLine("::---------------------------------------------------------------------------------------------------");
+        Console.WriteLine(":: Temps écoulé: {0:D2}min {1:D2}sec {2:D3}mm",
+            elapsed.Minutes,          // Minutes
+            elapsed.Seconds,          // Secondes
+            elapsed.Milliseconds);    // Millisecondes
+        Console.WriteLine("::---------------------------------------------------------------------------------------------------");
+    }
+
+    public static void Main()
+    {
+        OutputTitle();
+
+        // Démarre un chronomètre pour calculer le temps d'exécution 
+        Stopwatch stopwatch = new Stopwatch();
+        stopwatch.Start();
+
+        // Delete files before execution
+        if (File.Exists(fileData)) File.Delete(fileData);
+        if (File.Exists(finalFile)) File.Delete(finalFile);
+        if (File.Exists(fileBdd)) File.Delete(fileBdd);
+
+        ReadAllFile();
+        FileManagement();
+        CreateBdd();
+        
+        // Arrête le chronomètre
+        stopwatch.Stop();
+
+        // Récupère le temps écoulé
+        TimeSpan elapsed = stopwatch.Elapsed;
+        DisplayRuntime(elapsed);
+
+        BDDValueCheck.CBDDValueCheck.MainBDD();
+    }
+}
